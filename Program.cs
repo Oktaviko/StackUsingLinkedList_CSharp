8b7289295cd2e5217a01367c30eb5a5e6f974d6e
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2022-12-01 15:38:08 +0700

    Create function class Stacks

--- Program.cs
+++ Program.cs
@@ -16,6 +16,10 @@ namespace StackUsingLinkedList_CSharp
             next = n;
         }
     }
+    class Stacks
+    {
+
+    }
     internal class Program
     {
         static void Main(string[] args)
