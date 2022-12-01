4bc916a71ffd2ced805b10322045f8f6c705a533
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2022-12-01 15:37:29 +0700

    add variabel info and next in function class Node

--- Program.cs
+++ Program.cs
@@ -8,7 +8,13 @@ namespace StackUsingLinkedList_CSharp
 {
     class Node
     {
-
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i;
+            next = n;
+        }
     }
     internal class Program
     {
