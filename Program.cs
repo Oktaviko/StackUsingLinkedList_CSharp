3540bfc5cf4f6a3c9f0d8c6141c87caeabba8283
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2022-12-01 15:43:22 +0700

    add method push

--- Program.cs
+++ Program.cs
@@ -30,6 +30,15 @@ namespace StackUsingLinkedList_CSharp
             else
                 return(false);
         }
+        public void push(int element)
+        {
+            Node fresh;
+            fresh = new Node(element, null);
+
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + " pushed.");
+        }
     }
     internal class Program
     {
